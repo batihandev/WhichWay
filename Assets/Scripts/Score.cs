b3e0083d3849b4d300ce2cf3cdf3780a84b96a08
VexeDeity <batihan012@gmail.com>
2022-02-27 00:46:40 +0300

    ScoreInEndGame
    Score high score added for end game screen.

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+
+public class Score : MonoBehaviour
+{
+    public TextMeshProUGUI score;
+    public TextMeshProUGUI highScore;
+    int _highScore;
+    //int firstScore = 0;
+    // Start is called before the first frame update
+   
+    void Start()
+    {
+
+        
+        _highScore = PlayerPrefs.GetInt("highScore");
+        //firstScore = _highScore;
+        if (_highScore < Spawner.score)
+        {
+            PlayerPrefs.SetInt("highScore", Spawner.score);
+        }
+        score.text = "Score : " + Spawner.score;
+        _highScore = PlayerPrefs.GetInt("highScore");
+        highScore.text = "High Score : " + _highScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
